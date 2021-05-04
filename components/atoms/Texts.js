a44ca39f0f0871fd28292ee5f7176cc25ef23423
Osman Emin USTA <osmaneinusta@gmail.com>
2021-05-04 17:12:18 +0300

    add components

--- components/atoms/Texts.js
+++ components/atoms/Texts.js
@@ -3,14 +3,26 @@ import cn from "classnames";
 
 import styles from "./texts.module.css";
 
-export const TitleSm = ({ title }) => {
-  return <h6 className={styles.titleSm}>{title}</h6>;
+export const TitleSm = ({ title, className }) => {
+  return <h6 className={cn(styles.titleSm, className)}>{title}</h6>;
 };
 
 export const TitleMd = ({ title, className }) => {
   return <h4 className={cn(styles.titleMd, className)}>{title}</h4>;
 };
 
-export const TextSm = ({ text }) => {
-  return  <p className={styles.textSm}>{text}</p>;
+export const TitleLg = ({ title, className }) => {
+  return <h3 className={cn(styles.titleLg, className)}>{title}</h3>;
+};
+
+export const TitleXl = ({ title, className }) => {
+  return <h1 className={cn(styles.titleXl, className)}>{title}</h1>;
+};
+
+export const TextSm = ({ text, className }) => {
+  return <p className={cn(styles.textSm, className)}>{text}</p>;
+};
+
+export const TextMd = ({ text, className }) => {
+  return <p className={cn(styles.textMd, className)}>{text}</p>;
 };

92774f00515af23bb0543f0eaa47334bf68e9b9b
Osman Emin USTA <osmaneinusta@gmail.com>
2021-05-05 18:06:00 +0300

    add filter function

--- hooks/GetMovies.js
+++ hooks/GetMovies.js
@@ -1,18 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import getConfig from "next/config";
 
 export default function GetMovies() {
   const [movies, setMovies] = useState();
+  const [movieCategory, setMovieCategory] = useState("list/1?");
 
   const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
-  const url = `https://api.themoviedb.org/3/list/1?api_key=${publicRuntimeConfig.accessKey}`;
+  useEffect(() => {
+    getMoviesData();
+}, [movieCategory]);
 
-  const getMovies = () => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setMovies(data));
+  const getMoviesData = async () => {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/${movieCategory}api_key=${publicRuntimeConfig.accessKey}`
+    );
+    const moviesList = await res.json();
+    moviesList.items
+      ? setMovies(moviesList.items)
+      : setMovies(moviesList.results);
   };
 
-  return [movies, getMovies];
+  // https://api.themoviedb.org/3/search/movie?query=cars&api_key=4d94eea6262f660053da704fae1beb92
+
+  const setMoviesData = (ca) => {
+    console.log(ca);
+    setMovieCategory(ca);
+  };
+
+  return [movies, getMoviesData, movieCategory, setMoviesData];
 }

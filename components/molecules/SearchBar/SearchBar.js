6e7979f1d3b4f9953470cb1a4b73db7444d1aece
Osman Emin USTA <osmaneinusta@gmail.com>
2021-05-05 18:09:45 +0300

    added filter feature

--- components/molecules/SearchBar/SearchBar.js
+++ components/molecules/SearchBar/SearchBar.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {useContext} from "react";
 
 import styles from "./searchBar.module.css";
 
 import { Search } from "../../atoms/Images/Images";
 import { InputText } from "../../atoms/FormElements/FormElements";
+import { UserContext } from "../../../store/Context" 
 
 export default function SearchBar() {
+  const context = useContext(UserContext);
+  const inputRef = React.useRef();
+
+  const filterWithMovieName = () => {
+    context.setMovieCategory("search/movie?query="+inputRef.current.value+"&");
+  };
+
   return (
     <div className={styles.container}>
-      <Search />
-      <InputText className={styles.input} placeholder="Search By Title" />
+      {/* <Search onClick={filterWithMovieName()} /> */}
+      <img onClick={()=>filterWithMovieName()} src="/search.svg" width={25} height={25} />;
+      <input
+        ref={(node) => (inputRef.current = node)}
+        className={styles.input}
+        placeholder="Search By Title"
+      />
     </div>
   );
 }

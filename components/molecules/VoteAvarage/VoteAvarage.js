7963339a155c32e6614d41c0ef92a73bff367085
Osman Emin USTA <osmaneinusta@gmail.com>
2021-05-06 16:59:03 +0300

    Update VoteAvarage.js

--- components/molecules/VoteAvarage/VoteAvarage.js
+++ components/molecules/VoteAvarage/VoteAvarage.js
@@ -4,13 +4,13 @@ import cn from "classnames";
 import styles from "./voteAvarage.module.css";
 
 import { Star } from "../../atoms/Images/Images";
-import { TextSm } from "../../atoms/Texts/Texts";
+import { Text } from "../../atoms/Texts/Texts";
 
 export default function VoteAvarage({ voteAverage, className }) {
   return (
     <div className={cn(styles.container, className)}>
       <Star />
-      <TextSm className={styles.textSm} text={voteAverage} />
+      <Text className={styles.textSm} text={voteAverage} />
     </div>
   );
 }

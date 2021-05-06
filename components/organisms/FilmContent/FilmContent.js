1d57bdb20cf38e9be7566fc67bddd11c8e2ac582
Osman Emin USTA <osmaneinusta@gmail.com>
2021-05-06 16:58:49 +0300

    change movie data

--- components/organisms/FilmContent/FilmContent.js
+++ components/organisms/FilmContent/FilmContent.js
@@ -3,37 +3,37 @@ import React, { useContext } from "react";
 import styles from "./filmContent.module.css";
 
 import { Context } from "../../../store/Context";
-import { TitleXl, TitleLg, TextMd, TextSm } from "../../atoms/Texts/Texts";
+import { TitleXl, TitleLg, Text} from "../../atoms/Texts/Texts";
 import VoteAvarage from "../../molecules/VoteAvarage/VoteAvarage";
 
 export default function FilmContent() {
   const context = useContext(Context);
-  const release_date = context.character.release_date.split("-").reverse().toString().replace(/,/g,"/");
+  const release_date = context.movie.release_date?.split("-").reverse().toString().replace(/,/g,"/");
   
   let genres = [];
 
-  context.character.genres.map((item) => {
+  context.movie.genres?.map((item) => {
     genres = [...genres, " " + item.name]
   } )
 
   return (
     <div className={styles.container}>
-      <TitleXl className={styles.titleXl} title={context.character.title} />
-      <TitleLg title={context.character.tagline} />
-      <TextMd className={styles.textMd} text={release_date + " · " + genres }/>
+      <TitleXl className={styles.titleXl} title={context.movie.title} />
+      <TitleLg title={context.movie.tagline} />
+      <Text className={styles.textMd} text={release_date + " · " + genres }/>
       <div className={styles.voteContainer}>
-        <TextMd className={styles.textMd} text="User Score"/>
-        <VoteAvarage className={styles.voteAvarage} voteAverage={context.character.vote_average}/>
+        <Text className={styles.textMd} text="User Score"/>
+        <VoteAvarage className={styles.voteAvarage} voteAverage={context.movie.vote_average}/>
       </div>
       <div className={styles.voteContainer}>
-          <TextMd className={styles.textMd} text="Vote Count" />
-          <TextSm className={styles.textSm} text={context.character.vote_count}/>
+          <Text className={styles.textMd} text="Vote Count" />
+          <Text className={styles.textSm} text={context.movie.vote_count}/>
       </div>
-      <a href={context.character.homepage}>
-          <TextMd className={styles.linkedText} text="Home Page"/>
+      <a href={context.movie.homepage}>
+          <Text className={styles.linkedText} text="Home Page"/>
       </a>
       <TitleLg title="Overview" />
-      <TextMd className={styles.textMd} text={context.character.overview}/>
+      <Text className={styles.textMd} text={context.movie.overview}/>
     </div>
   );
 }

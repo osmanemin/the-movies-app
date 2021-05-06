f42e1df3653a7c7f514b7742f3e5574426a979ec
Osman Emin USTA <osmaneinusta@gmail.com>
2021-05-06 16:49:24 +0300

    add props

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import Index from "../components/templates/Index/Index";
 
 function HomePage() {
-  return <Index></Index>
+  return <Index category="list/1?"></Index>
 }
 
 export default HomePage;

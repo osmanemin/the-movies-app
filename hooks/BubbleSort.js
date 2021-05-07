a749c8650a859ed561720e84d15f72dab4a71daf
Osman Emin USTA <45307514+osmanemin@users.noreply.github.com>
2021-05-07 14:43:58 +0300

    Update BubbleSort.js

--- hooks/BubbleSort.js
+++ hooks/BubbleSort.js
@@ -1,18 +1,14 @@
 export const BubbleSort = (array) => {
   let tmp;
 
-  if (array === undefined) 
-    return [];
+  if (array === undefined) return [];
 
-    console.log(array[1].vote_average)
   for (let i = 0; i < array.length; i++) {
-    for (let j = 1; j < array.length - i ; j++) {
-        console.log(array[j].vote_average)
-
-      if (array[j-1].vote_average < array[j].vote_average) {
+    for (let j = 1; j < array.length - i; j++) {
+      if (array[j - 1].vote_average < array[j].vote_average) {
         tmp = array[j];
-        array[j] = array[j-1];
-        array[j-1] = tmp;
+        array[j] = array[j - 1];
+        array[j - 1] = tmp;
       }
     }
   }

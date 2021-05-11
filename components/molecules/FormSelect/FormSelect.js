ebf10cec0a572950920fe96722219614ba64f239
Osman Emin USTA <45307514+osmanemin@users.noreply.github.com>
2021-05-11 13:56:30 +0300

    add key to option tag

--- components/molecules/FormSelect/FormSelect.js
+++ components/molecules/FormSelect/FormSelect.js
@@ -10,8 +10,8 @@ export default function FormSelect({ title, setSortType, values }) {
     <div>
       <label className={styles.title}>{title}</label>
       <select className={styles.optionsContainer} onChange={sortingTypeChange}>
-          {values.map((value) => {
-             return <option value={value}>{value}</option>
+          {values.map((value, index) => {
+             return <option key={index} value={value}>{value}</option>
           })}
       </select>
     </div>

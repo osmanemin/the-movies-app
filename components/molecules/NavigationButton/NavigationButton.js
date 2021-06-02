4e0898499764992c28c2b3eef25e917c5059e610
Osman Emin USTA <45307514+osmanemin@users.noreply.github.com>
2021-06-02 18:19:43 +0300

    change styles

--- components/molecules/NavigationButton/NavigationButton.js
+++ components/molecules/NavigationButton/NavigationButton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
 import styles from "./navigationButton.module.css";
@@ -6,10 +6,17 @@ import styles from "./navigationButton.module.css";
 import { TitleMd } from "../../atoms/Texts/Texts";
 
 export default function NavigationButton({ children, title, href }) {
+  const [color, setColor] = useState("");
+
+  useEffect(() => {
+    "/" + window.location.href.split("/").slice(-1)[0] === href &&
+      setColor("white");
+  }, []);
+
   return (
     <Link href={href}>
       <a>
-        <div className={styles.container}>
+        <div className={styles.container} style={{ borderColor: `${color}` }}>
           {children}
           <TitleMd className={styles.title} title={title} />
         </div>

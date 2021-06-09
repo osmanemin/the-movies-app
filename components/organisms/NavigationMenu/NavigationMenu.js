8c64b9b525966c2f5a53d4efe2299f38ebb349bd
Osman Emin USTA <45307514+osmanemin@users.noreply.github.com>
2021-06-09 11:50:44 +0300

    add favorites

--- components/organisms/NavigationMenu/NavigationMenu.js
+++ components/organisms/NavigationMenu/NavigationMenu.js
@@ -1,28 +1,40 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import styles from "./navigationMenu.module.css";
 
-import { MostPopular, FMIcon } from "../../atoms/Images/Images";
+import { NavigationIcon, FMIcon } from "../../atoms/Images/Images";
 import NavigationButton from "../../molecules/NavigationButton/NavigationButton";
 
-export default function NavigationMenu() {
+const NavigationMenu = ({favoritesList}) => {
   return (
     <div className={styles.container}>
       <FMIcon className={styles.container} />
       <div className={styles.menu}>
         <NavigationButton title="Most Popular" href="/">
-          <MostPopular />
+          <NavigationIcon src="/most_popular.svg" />
         </NavigationButton>
         <NavigationButton title="Upcoming" href="/upcoming">
-          <MostPopular />
+          <NavigationIcon src="/upcoming.svg" />
         </NavigationButton>
         <NavigationButton title="Top Rated" href="/top-rated">
-          <MostPopular />
+          <NavigationIcon src="/top-rated.svg" />
         </NavigationButton>
         <NavigationButton title="Trending" href="/trending">
-          <MostPopular />
+          <NavigationIcon src="/trending.svg" />
+        </NavigationButton>
+        <NavigationButton title={`Favorites    ${favoritesList.length || ""}`} href="/favorites">
+          <NavigationIcon src="/favorite.svg" />
         </NavigationButton>
       </div>
     </div>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    favoritesList: state.favoritesList,
+  };
+};
+
+export default connect(mapStateToProps)(NavigationMenu);

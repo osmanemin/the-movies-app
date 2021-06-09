75699e439a31657b245382b168bf37e874d70b7b
Osman Emin USTA <45307514+osmanemin@users.noreply.github.com>
2021-06-09 11:49:30 +0300

    csr to ssr

--- pages/search.js
+++ pages/search.js
@@ -1,39 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import getConfig from "next/config";
 
 import { UserContext } from "../store/Context";
 import Index from "../components/templates/Index/Index";
 
-export default function Search() {
-  const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
-
-  const [moviesInState, setMoviesInState] = useState([]);
-
-  let movies;
-
-  const [query, setQuery] = useState("");
-
-  useEffect(() => {
-    window.location.search.includes("=") && setQuery(window.location.search.split("=")[1]);
-  });
-
-  useEffect(() => {
-    !query || getMovies();
-  }, [query]);
-
-  const getMovies = async () => {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${publicRuntimeConfig.accessKey}`
-    );
-
-    const moviesList = await res.json();
-    movies = moviesList.results;
-    setMoviesInState(movies);
-  };
+export default function Search({ data }) {
+  const [moviesInState, setMoviesInState] = useState(data);
 
   return (
-    <UserContext.Provider value={{ movies, moviesInState, setMoviesInState }}>
+    <UserContext.Provider
+      value={{ movies: data, moviesInState, setMoviesInState }}
+    >
       <Index />
     </UserContext.Provider>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
+
+  const data = await fetch(
+    `${serverRuntimeConfig.baseDomain}/api/search?q=${context.query.q}`
+  ).then((res) => res.json());
+  return { props: { data } };
+};

bc2af8ab2bdcd08b6625bb6e404ee4c72711693e
Osman Emin USTA <45307514+osmanemin@users.noreply.github.com>
2021-06-09 11:46:12 +0300

    add redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
-import { useEffect } from "react";
-
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "../reducers/reducers";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const store = createStore(reducer);
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
